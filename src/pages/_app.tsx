45a49ca787f614148b3c87cf7cee56f80316bd3b
Alejandro Dotor <alejandrodotor8@gmail.com>
2023-03-27 12:27:17 -0500

    feat: add poppins font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { SessionProvider, useSession } from 'next-auth/react';
-import '@/styles/globals.scss';
 import { AuthProvider } from '@/hooks/useAuth';
+import { Poppins } from 'next/font/google';
+import '@/styles/globals.scss';
+
+const poppins = Poppins({
+	weight: ['400', '500', '700'],
+	style: ['normal'],
+	subsets: ['latin'],
+});
 
 export default function MyApp({
 	Component,
@@ -11,13 +18,15 @@ export default function MyApp({
 	return (
 		<SessionProvider session={session}>
 			<AuthProvider>
-				{Component.auth ? (
-					<Auth>
+				<div className={poppins.className}>
+					{Component.auth ? (
+						<Auth>
+							<Component {...pageProps} />
+						</Auth>
+					) : (
 						<Component {...pageProps} />
-					</Auth>
-				) : (
-					<Component {...pageProps} />
-				)}
+					)}
+				</div>
 			</AuthProvider>
 		</SessionProvider>
 	);
