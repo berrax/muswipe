3e766927ddeb2b63b61b446b6e4541cc85816ad6
Alejandro Dotor <alejandrodotor8@gmail.com>
2023-03-29 09:39:28 -0500

    refactor: optimize useTheme

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,10 +1,4 @@
-import {
-	createContext,
-	useContext,
-	useState,
-	useEffect,
-	useCallback,
-} from 'react';
+import { createContext, useContext, useState, useCallback } from 'react';
 import { IPropsChildren, ITheme } from '@/interfaces/globals.interface';
 
 export const ThemeContext = createContext<ITheme>(null as any);
@@ -19,35 +13,41 @@ export const ThemeProvider = ({ children }: IPropsChildren) => {
 export const useTheme = () => useContext(ThemeContext);
 
 function useProviderTheme() {
-	const [isDarkTheme, setIsDarkTheme] = useState(false);
+	const [isDarkTheme, setIsDarkTheme] = useState(initIsDarkTheme());
 
 	const toggleTheme = useCallback(() => {
 		setIsDarkTheme(isDark => {
 			if (isDark) window.localStorage.setItem('IS_DARK_THEME', 'false');
 			else window.localStorage.setItem('IS_DARK_THEME', 'true');
+			handleChangeDark(!isDark);
 			return !isDark;
 		});
 	}, []);
 
-	useEffect(() => {
-		if (isDarkTheme) {
-			document.documentElement.dataset.theme = 'dark';
-		} else {
-			document.documentElement.dataset.theme = 'light';
-		}
-	}, [isDarkTheme]);
-
-	useEffect(() => {
-		const isDarkThemeLS = window.localStorage.getItem('IS_DARK_THEME');
-		if (isDarkThemeLS) {
-			setIsDarkTheme(isDarkThemeLS === 'true');
-		} else {
-			const prefersDarkTheme = window.matchMedia(
-				'(prefers-color-scheme: dark)',
-			).matches;
-			setIsDarkTheme(prefersDarkTheme);
-		}
-	}, []);
-
 	return { isDarkTheme, toggleTheme };
+}
+
+function handleChangeDark(isDark: boolean) {
+	if (isDark) {
+		document.documentElement.dataset.theme = 'dark';
+	} else {
+		document.documentElement.dataset.theme = 'light';
+	}
+}
+
+function initIsDarkTheme() {
+	if (typeof window === 'undefined') return false;
+
+	const IS_DARK_THEME_LS = localStorage.getItem('IS_DARK_THEME');
+	if (IS_DARK_THEME_LS) {
+		const isDarkTheme = IS_DARK_THEME_LS === 'true';
+		handleChangeDark(isDarkTheme);
+		return isDarkTheme;
+	}
+
+	const prefersDarkTheme = window.matchMedia(
+		'(prefers-color-scheme: dark)',
+	).matches;
+	handleChangeDark(prefersDarkTheme);
+	return prefersDarkTheme;
 }
